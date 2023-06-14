6089fd612be30cb8ac7567fc6790fd8e66775b5a
Adedolapo Olutuyo <85107972+tuyojr@users.noreply.github.com>
2023-06-14 11:08:37 +0000

    SQL Basic Select challenge solution

--- weather_observation_station_3.sql
+++ weather_observation_station_3.sql
@@ -1,3 +1,10 @@
--- Query a list of CITY names from STATION for cities that have 
--- an even ID number. Print the results in any order, but exclude 
--- duplicates from the answer.
+/*
+Query a list of CITY names from STATION for cities that have 
+an even ID number. Print the results in any order, but exclude 
+duplicates from the answer.
+
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+
+SELECT DISTINCT CITY FROM STATION WHERE ID % 2 = 0;
