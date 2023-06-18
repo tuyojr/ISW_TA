64287646f487960ae1314a97906c9adc6ed5fcc8
tuyojr <olutuyod@gmail.com>
2023-06-18 10:01:14 +0100

    population density difference query solution

---
+++ SQL/population_density_diff.sql
@@ -0,0 +1,8 @@
+/*
+Query the difference between the maximum and minimum populations in CITY.
+
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+
+SELECT MAX(POPULATION) - MIN(POPULATION) FROM CITY;
