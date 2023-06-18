083db30ce2eb8562c0ff5f06b34b0e9ac7c3d707
tuyojr <olutuyod@gmail.com>
2023-06-18 09:56:18 +0100

    Revising Aggregations - Averages query solution

---
+++ SQL/revising_aggregations_III.sql
@@ -0,0 +1,8 @@
+/*
+Query the average population of all cities in CITY where District is California.
+
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+
+SELECT AVG(POPULATION) FROM CITY WHERE DISTRICT = 'California';
