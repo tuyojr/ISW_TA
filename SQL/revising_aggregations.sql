431aa4658a642d866b7d0e76dc0565c255ccbbd0
tuyojr <olutuyod@gmail.com>
2023-06-18 09:52:12 +0100

    Revising Aggregations - The Count Function query solution

---
+++ SQL/revising_aggregations.sql
@@ -0,0 +1,8 @@
+/*
+Query a count of the number of cities in CITY having a Population larger than 100,000.
+
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+
+SELECT COUNT(COUNTRYCODE) FROM CITY WHERE POPULATION > 100000;
