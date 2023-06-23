c72c48f6a2202cbff17ceb4bf110e9fbe2f8b08c
Adedolapo Olutuyo <Adedolapo.Olutuyo@Interswitchgroup.com>
2023-06-23 09:55:07 +0100

    working with the return keyword to return nothing

--- python/demo.py
+++ python/demo.py
@@ -435,3 +435,10 @@
 
 # x = sum(1, 2)
 # print("Sum of two numbers is: ", x)
+
+# return keyword returning nothing ---> returns "None"
+def m1():
+    print("This function is returning nothing.")
+
+x = m1()
+print(x)
