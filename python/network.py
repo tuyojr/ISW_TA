bf6041e8e8aa38c1b5bc7448669b3fb79c8212bd
tuyojr <olutuyod@gmail.com>
2023-07-03 15:30:53 +0100

    creating a simple server and client chatbot

--- python/network.py
+++ python/network.py
@@ -26,3 +26,55 @@
 # s.connect((host, port))
 # print(s.recv(1024).decode())
 # s.close()
+
+# # server side
+# import socket # imports the socket module
+
+# def server_program():
+#     host = "192.168.45.120" # gets the local machine name (server name)
+#     port = 1025 # reserve a port for your service to connect on
+
+#     s = socket.socket() # creates a socket object
+#     s.bind((host, port)) # binds to the port
+
+#     s.listen(2) # listens for connections
+#     conn, addr = s.accept() # establishes connection with client
+#     print("Connection from:" + str(addr))
+
+#     while True:
+#         # receive data stream. it wont accept
+#         data = conn.recv(1024).decode()
+#         if not data:
+#             # if data is not received, break
+#             break
+#         print("from connected user: " + str(data))
+#         data = input(" -> ")
+#         conn.send(data.encode()) # send data to the client
+#     conn.close() # closes the connection
+# if __name__ == '__main__':
+#     server_program()
+
+# # client side
+# import socket # imports the socket module
+
+# def client_program():
+#     host = "192.168.45.120" # gets the local machine name (server name)
+#     port = 1025 # reserve a port for your service to connect on
+
+#     s = socket.socket() # creates a socket object
+#     s.connect((host, port)) # connects to the server
+
+#     message = input(" -> ") # takes input
+
+#     while message.lower().strip() != 'bye':
+#         s.send(message.encode())
+#         data = s.recv(1024).decode()
+
+#         print("Received from server: " + data)
+
+#         message = input(" -> ")
+    
+#     s.close() # closes the connection
+
+# if __name__ == '__main__':
+#     client_program()
