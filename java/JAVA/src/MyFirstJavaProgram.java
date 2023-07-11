081b98e598530db74a4617f0188e6e233325affc
tuyojr <olutuyod@gmail.com>
2023-07-11 16:02:59 +0100

    working with data types and string methods

--- java/JAVA/src/MyFirstJavaProgram.java
+++ java/JAVA/src/MyFirstJavaProgram.java
@@ -1,5 +1,48 @@
 public class MyFirstJavaProgram{
 	public static void main(String args[]){
-		System.out.println("Omoooor! Na so I start Java!");
+		// System.out.println("Omoooor! Na so I start Java!");
+		
+		// working with integer primitive data types
+		// byte myNum = 100;
+		// short myNum = 5000;	// numbers between -32768 to 32767
+		// int myNum = 100000;
+		// long myNum = 150000000000L;
+		// float myNum = 5.75f;
+		// double myNum = 19.99d;
+		// System.out.println(myNum);
+
+		// working with data types cont'd
+		// char myGrade = 'B';
+		// System.out.println(myGrade);
+
+		// char myVar1 = 65, myVar2 = 66, myVar3 = 67, myVar4 = 55;
+		// System.out.println(myVar1);
+		// System.out.println(myVar2);
+		// System.out.println(myVar3);
+		// System.out.println(myVar4);
+		
+//		//non-primitive or reference data types Arrays, Strings, Classes, Interface.
+//		String greeting = "Wagwan my brudda!";
+//
+//		// string methods
+//		// length() method helps to get the length of a string.
+//		System.out.println(greeting);
+//		System.out.println("The total length of the string is: " + greeting.length());
+//
+//		// toUpperCase() method helps to convert a string to uppercase.
+//		System.out.println(greeting.toUpperCase());
+//
+//		// toLowerCase() method helps to convert a string to lowercase.
+//		System.out.println(greeting.toLowerCase());
+//
+//		// indexOf() method helps to find the index of a character in a string. Always returns the
+//		// first occurence of the character in the string.
+//		System.out.println(greeting.indexOf("my"));
+
+//		// String concatenation
+//		String firstName = "Shisui";
+//		String lastName = "Uchiha";
+//		System.out.println(firstName + " " + lastName);
+//		System.out.println(firstName.concat(" " + lastName));
 	}
 }
