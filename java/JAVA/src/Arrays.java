189b03fd5639efd562a6a8dcec4fe67d6085e7bf
tuyojr <olutuyod@gmail.com>
2023-07-12 15:55:14 +0100

    getting started with arrays

---
+++ java/JAVA/src/Arrays.java
@@ -0,0 +1,22 @@
+public class Arrays {
+    public static void main(String[] args) {
+//        int[] a = new int[5]; // declare an array of 5 integers
+//        // initialize the array
+//        a[0] = 10;
+//        a[1] = 20;
+//        a[2] = 30;
+//        a[3] = 40;
+//        a[4] = 50;
+//        int[] b = {10, 20, 30, 40, 50}; // declare and initialize an array
+//
+//        // traverse the array
+//        for(int i = 0; i < a.length; i++) {
+//            System.out.println(a[i]);
+//        }
+//
+//        // enhanced for loop
+//        for(int i : b) {
+//            System.out.println(i);
+//        }
+    }
+}
