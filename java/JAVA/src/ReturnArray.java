e631ccd787f7fedae049a59caa65c0d9454e20e0
tuyojr <olutuyod@gmail.com>
2023-07-12 16:30:58 +0100

    return array

---
+++ java/JAVA/src/ReturnArray.java
@@ -0,0 +1,17 @@
+public class ReturnArray {
+    static int[] get() {
+        return new int[] { 10, 22, 44, 66 };
+    }
+
+    public static void main(String[] args) {
+        int[] arr = get();
+        for (int j : arr) System.out.print(j + " ");
+
+        // ArrayIndexOutOfBoundsException
+        int[] a = get();
+        for(int i = 0; i <= a.length; i++){
+            System.out.print(i); // 01234
+            System.out.println(a[i]; // 10 22 44 66 Exception
+        }
+    }
+}
