5823ebd78cf1ee7bbee33bf29f2ae8c6489dfa44
tuyojr <olutuyod@gmail.com>
2023-07-12 14:49:37 +0100

    creating nested loops to print a tree

--- java/JAVA/src/Looper.java
+++ java/JAVA/src/Looper.java
@@ -7,4 +7,30 @@ public class Looper {
 //        }
 //        System.out.println();
 //    }
+
+    // nested for loop
+    public static void main(String[] args) {
+//        for (int i = 1; i <= 3; i ++) {
+//            for (int j = 1; j <= 3; j ++) {
+//                System.out.print((i + j) + " ");
+//            }
+//            System.out.println();
+//        }
+//        // nested for loop II
+//        for (int i = 1; i <= 5; i ++) {
+//            for (int j = 1; j <= i; j ++) {
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+
+        // nested for loop III
+        // reverse the order of the nested loop
+        for (int i = 1; i <= 5; i ++) {
+            for (int j = 5; j >= i; j --) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
 }
