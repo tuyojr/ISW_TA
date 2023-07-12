750ce86eae6ee55d53e08905b7df94b8605c08d0
tuyojr <olutuyod@gmail.com>
2023-07-12 12:31:11 +0100

    creating a simple for loop

---
+++ java/JAVA/src/Looper.java
@@ -0,0 +1,10 @@
+public class Looper {
+//    // simple for loop
+//    public static void main(String[] args) {
+//        for (int i = 1; i <= 10; i++) {
+//            if (i < 10) System.out.print(i + ", ");
+//            else System.out.println(i + ".");
+//        }
+//        System.out.println();
+//    }
+}
