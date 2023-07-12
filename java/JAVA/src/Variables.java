c600d9343c179a79cac8ec88e8d2ff1d5a10dfde
tuyojr <olutuyod@gmail.com>
2023-07-12 10:07:40 +0100

    simple calss activity to work with variables

---
+++ java/JAVA/src/Variables.java
@@ -0,0 +1,18 @@
+public class Variables {
+    public static void main(String [] args) {
+//        // final keyword makes a variable constant
+//        final int num1;
+//        num1 = 15;
+//        System.out.println(num1);
+
+//        // create a variable named carName and assign the value Volvo to it.
+//        int num1 = 5;
+//        float myFloatNum = 5.99f;
+//        char myLetter = 'D';
+//        boolean myBool = true;
+//        String myText = "Hello";
+//        String carName = "Volvo";
+//
+//        System.out.println(myText.concat(" " + carName + "."));
+    }
+}
