4ce7932f0932591970f046ff3e703e556610a3b1
tuyojr <olutuyod@gmail.com>
2023-07-13 16:56:59 +0100

    creating a collection with maps

---
+++ java/JAVA/src/CollectionsMap.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class CollectionsMap {
+    public static void main(String[] args) {
+        Map map = new HashMap();
+        map.put("10", "Kol");
+        map.put("27", "Finn");
+        map.put("77", "Ziggy");
+        map.put("4", "Pendagast");
+
+        System.out.println();
+        System.out.println("Map Elements");
+        System.out.println("\t" + map);
+    }
+}
