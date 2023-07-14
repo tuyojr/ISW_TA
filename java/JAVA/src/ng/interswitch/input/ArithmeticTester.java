e943733ecbecf42db38a7ed738e410a3a417393f
tuyojr <olutuyod@gmail.com>
2023-07-14 11:21:50 +0100

    testing basic aritmethic on user input

---
+++ java/JAVA/src/ng/interswitch/input/ArithmeticTester.java
@@ -0,0 +1,29 @@
+package ng.interswitch.input;
+import java.util.Scanner;
+
+public class ArithmeticTester {
+    public static void main(String[] args) {
+        // create a scanner object to read input from the command line
+        try(Scanner userInput = new Scanner(System.in)) {
+
+            // prompt the user to enter two numbers
+            System.out.println("Enter two numbers:");
+
+            // read the two numbers from the command line; store them in variables
+            // nextDouble() reads the next double from the command line
+            double firstNumber = userInput.nextDouble();
+            double secondNumber = userInput.nextDouble();
+
+            // create an object of the BasicArithmetic class and pass the two numbers to the constructor
+            BasicArithmetic basicArithmetic = new BasicArithmetic(firstNumber, secondNumber);
+
+            // print the sum and difference of the two numbers
+            System.out.printf("Performing addition operation: %s + %s \n", firstNumber, secondNumber);
+            System.out.println("The sum of the two numbers is: " + basicArithmetic.addFirstAndSecondNumber());
+            System.out.printf("Performing subtraction operation: %s - %s \n", firstNumber, secondNumber);
+            System.out.println("The difference of the two numbers is: " + basicArithmetic.subtractFirstAndSecondNumber());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
