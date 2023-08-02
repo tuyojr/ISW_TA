1fb4cd907171e5433ee5d62333fdb4296a26d44b
tuyojr <olutuyod@gmail.com>
2023-08-02 14:03:11 +0100

    department table

---
+++ java/juniorbackend/src/main/java/com/company/juniorbackend/model/Department.java
@@ -0,0 +1,26 @@
+package com.company.juniorbackend.model;
+
+import lombok.*;
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Department")
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "manager_id")
+    private Employee manager;
+}
