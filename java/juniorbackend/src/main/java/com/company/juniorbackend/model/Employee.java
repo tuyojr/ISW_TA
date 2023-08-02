e9b4da975df260bfe97f9cc0528a58819cd8f1ae
tuyojr <olutuyod@gmail.com>
2023-08-02 14:05:40 +0100

    employee table

---
+++ java/juniorbackend/src/main/java/com/company/juniorbackend/model/Employee.java
@@ -0,0 +1,41 @@
+package com.company.juniorbackend.model;
+
+import lombok.*;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer phone;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private LocalDateTime dateOfBirth;
+
+    @Column(nullable = false)
+    private LocalDateTime joinedDate;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Department department;
+
+    @ManyToOne
+    @JoinColumn(name = "employee_category_id")
+    private EmployeeCategory employeeCategory;
+}
