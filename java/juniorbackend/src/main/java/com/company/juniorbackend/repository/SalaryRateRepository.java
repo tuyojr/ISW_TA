6bf08040fa2b92c822b8162b69a700354c40edab
tuyojr <olutuyod@gmail.com>
2023-08-02 14:11:43 +0100

    salary rate repository

---
+++ java/juniorbackend/src/main/java/com/company/juniorbackend/repository/SalaryRateRepository.java
@@ -0,0 +1,7 @@
+package com.company.juniorbackend.repository;
+
+import com.company.juniorbackend.model.SalaryRate;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SalaryRateRepository extends JpaRepository<SalaryRate, Long> {
+}
