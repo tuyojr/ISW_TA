5478c1dbe3f3f1a61358d082b773b067438b7bdd
tuyojr <olutuyod@gmail.com>
2023-08-02 15:46:36 +0100

    employee repository to find by dept, manager, and salary rate ID

--- java/juniorbackend/src/main/java/com/company/juniorbackend/repository/EmployeeRepository.java
+++ java/juniorbackend/src/main/java/com/company/juniorbackend/repository/EmployeeRepository.java
@@ -3,7 +3,11 @@ package com.company.juniorbackend.repository;
 import com.company.juniorbackend.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.*;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    List<Employee> findByDepartmentId(Long departmentId);
+    List<Employee> findByManagerId(Long managerId);
+    List<Employee> findBySalaryRateId(Long rateId);
 }
