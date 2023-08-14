d85d825224aca683c1473ba3fe5952081a815256
tuyojr <olutuyod@gmail.com>
2023-08-14 13:26:58 +0100

    testing the sample project as a dependency

---
+++ java/sample-project-test/src/main/java/com/interswitch/maven/App.java
@@ -0,0 +1,14 @@
+package com.interswitch.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Running the arithmetic calculator..." );
+        User.collectUserInput();
+    }
+}
