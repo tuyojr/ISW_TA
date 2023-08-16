7f8ff9e73fe6dba97ace4ae6ffbc9790755ef3cf
tuyojr <olutuyod@gmail.com>
2023-08-16 12:22:50 +0100

    creating junit test

---
+++ java/lifecycle-test/src/main/java/com/interswitch/tests/App.java
@@ -0,0 +1,13 @@
+package com.interswitch.tests;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
